99749a7899d8e3ed38de55c1b3eb1d3b2705c8b8
redlightning <redlightning_user@protonmail.com>
2024-05-12 15:43:58 +0300

    Refactor replay queue save/load

--- src/env_thread/agent/replay/replay_queue.rs
+++ src/env_thread/agent/replay/replay_queue.rs
@@ -7,6 +7,86 @@ use std::collections::{HashMap, VecDeque};
 use std::path::Path;
 use std::rc::Rc;
 
+fn frames_transitions_serialized(
+    values: &VecDeque<Transition>,
+) -> (Vec<&Rc<ImageOwned2>>, Vec<SavedTransition>) {
+    let mut frames = vec![];
+    let mut transitions: Vec<SavedTransition> = vec![];
+    let mut frame_pointers_to_indices = HashMap::new();
+    let mut current_frame_index = 0;
+    for transition in values {
+        let mut state_frame_indices = vec![];
+        for frame in &transition.state {
+            let frame_index =
+                if let Some(frame_index) = frame_pointers_to_indices.get(&Rc::as_ptr(frame)) {
+                    *frame_index
+                } else {
+                    frames.push(frame);
+                    frame_pointers_to_indices.insert(Rc::as_ptr(frame), current_frame_index);
+                    let frame_index = current_frame_index;
+                    current_frame_index += 1;
+                    frame_index
+                };
+            state_frame_indices.push(frame_index);
+        }
+        let mut next_state_frame_indices = vec![];
+        for frame in &transition.next_state {
+            let frame_index =
+                if let Some(frame_index) = frame_pointers_to_indices.get(&Rc::as_ptr(frame)) {
+                    *frame_index
+                } else {
+                    frames.push(frame);
+                    frame_pointers_to_indices.insert(Rc::as_ptr(frame), current_frame_index);
+                    let frame_index = current_frame_index;
+                    current_frame_index += 1;
+                    frame_index
+                };
+            next_state_frame_indices.push(frame_index);
+        }
+        let transition = SavedTransition {
+            state_frame_indices: (*state_frame_indices).try_into().unwrap(),
+            next_state_frame_indices: (*next_state_frame_indices).try_into().unwrap(),
+            action: transition.action,
+            reward: transition.reward,
+            terminated: transition.terminated,
+        };
+        transitions.push(transition);
+    }
+    (frames, transitions)
+}
+
+fn values_deserialized<P: AsRef<Path>>(path: P, max_size: usize) -> Vec<Transition> {
+    let path = path.as_ref();
+    let mut frames_file = open_file_buf_read(path.join("frames")).unwrap();
+    let mut frames: Vec<Rc<ImageOwned2>> = vec![];
+    while has_data_left(&mut frames_file).unwrap() {
+        let frame = bincode::deserialize_from(&mut frames_file).unwrap();
+        let frame = Rc::new(frame);
+        frames.push(frame);
+    }
+    let mut transitions = Vec::with_capacity(max_size);
+    let mut transitions_file = open_file_buf_read(path.join("transitions")).unwrap();
+    while has_data_left(&mut transitions_file).unwrap() {
+        let saved_transition: SavedTransition =
+            bincode::deserialize_from(&mut transitions_file).unwrap();
+        let state = saved_transition
+            .state_frame_indices
+            .map(|frame_index| Rc::clone(&frames[frame_index]));
+        let next_state = saved_transition
+            .next_state_frame_indices
+            .map(|frame_index| Rc::clone(&frames[frame_index]));
+        let transition = Transition {
+            state,
+            next_state,
+            action: saved_transition.action,
+            reward: saved_transition.reward,
+            terminated: saved_transition.terminated,
+        };
+        transitions.push(transition);
+    }
+    transitions
+}
+
 pub struct ReplayQueue {
     transitions: VecDeque<Transition>,
     max_size: usize,
@@ -37,93 +117,27 @@ impl ReplayQueue {
         self.transitions.len()
     }
     pub fn save<P: AsRef<Path>>(&self, path: P) {
-        let mut frames = vec![];
-        let mut transitions: Vec<SavedTransition> = vec![];
-        let mut frame_pointers_to_indices = HashMap::new();
-        let mut current_frame_index = 0;
-        for transition in &self.transitions {
-            let mut state_frame_indices = vec![];
-            for frame in &transition.state {
-                let frame_index =
-                    if let Some(frame_index) = frame_pointers_to_indices.get(&Rc::as_ptr(frame)) {
-                        *frame_index
-                    } else {
-                        frames.push(frame);
-                        frame_pointers_to_indices.insert(Rc::as_ptr(frame), current_frame_index);
-                        let frame_index = current_frame_index;
-                        current_frame_index += 1;
-                        frame_index
-                    };
-                state_frame_indices.push(frame_index);
-            }
-            let mut next_state_frame_indices = vec![];
-            for frame in &transition.next_state {
-                let frame_index =
-                    if let Some(frame_index) = frame_pointers_to_indices.get(&Rc::as_ptr(frame)) {
-                        *frame_index
-                    } else {
-                        frames.push(frame);
-                        frame_pointers_to_indices.insert(Rc::as_ptr(frame), current_frame_index);
-                        let frame_index = current_frame_index;
-                        current_frame_index += 1;
-                        frame_index
-                    };
-                next_state_frame_indices.push(frame_index);
-            }
-            let transition = SavedTransition {
-                state_frame_indices: (*state_frame_indices).try_into().unwrap(),
-                next_state_frame_indices: (*next_state_frame_indices).try_into().unwrap(),
-                action: transition.action,
-                reward: transition.reward,
-                terminated: transition.terminated,
-            };
-            transitions.push(transition);
-        }
         // the experience replay queue can take up a lot of space, therefore we serialize each
         // frame/transition separately in a streaming manner so as to not inadvertently clone
         // the entire queue (which would cause a spike in RAM usage and might result in OOM)
         let max_size_file = create_file_buf_write(path.as_ref().join("max_size")).unwrap();
         bincode::serialize_into(max_size_file, &self.max_size).unwrap();
+        let (serialized_frames, serialized_transitions) =
+            frames_transitions_serialized(&self.transitions);
         let mut frames_file = create_file_buf_write(path.as_ref().join("frames")).unwrap();
-        for frame in frames {
+        for frame in serialized_frames {
             bincode::serialize_into(&mut frames_file, &**frame).unwrap();
         }
         let mut transitions_file =
             create_file_buf_write(path.as_ref().join("transitions")).unwrap();
-        for transition in transitions {
+        for transition in serialized_transitions {
             bincode::serialize_into(&mut transitions_file, &transition).unwrap();
         }
     }
     pub fn load<P: AsRef<Path>>(&mut self, path: P) {
         let max_size_file = open_file_buf_read(path.as_ref().join("max_size")).unwrap();
         self.max_size = bincode::deserialize_from(max_size_file).unwrap();
-        let mut frames_file = open_file_buf_read(path.as_ref().join("frames")).unwrap();
-        let mut frames: Vec<Rc<ImageOwned2>> = vec![];
-        while has_data_left(&mut frames_file).unwrap() {
-            let frame = bincode::deserialize_from(&mut frames_file).unwrap();
-            let frame = Rc::new(frame);
-            frames.push(frame);
-        }
-        let mut transitions = VecDeque::with_capacity(self.max_size);
-        let mut transitions_file = open_file_buf_read(path.as_ref().join("transitions")).unwrap();
-        while has_data_left(&mut transitions_file).unwrap() {
-            let saved_transition: SavedTransition =
-                bincode::deserialize_from(&mut transitions_file).unwrap();
-            let state = saved_transition
-                .state_frame_indices
-                .map(|frame_index| Rc::clone(&frames[frame_index]));
-            let next_state = saved_transition
-                .next_state_frame_indices
-                .map(|frame_index| Rc::clone(&frames[frame_index]));
-            let transition = Transition {
-                state,
-                next_state,
-                action: saved_transition.action,
-                reward: saved_transition.reward,
-                terminated: saved_transition.terminated,
-            };
-            transitions.push_back(transition);
-        }
-        self.transitions = transitions;
+        let deserialized_values = values_deserialized(path.as_ref(), self.max_size);
+        self.transitions = deserialized_values.into();
     }
 }
