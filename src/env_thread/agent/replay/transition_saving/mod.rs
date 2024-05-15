54d377e1a6430f9c0f2d530ba0c64bd2b40442db
redlightning <redlightning_user@protonmail.com>
2024-05-15 05:23:45 +0300

    Refactor transition serialization

--- src/env_thread/agent/replay/transition_saving/mod.rs
+++ src/env_thread/agent/replay/transition_saving/mod.rs
@@ -1,61 +1,10 @@
+mod transition_serializer;
+
 use crate::env_thread::{CompressedImageOwned2, CompressedTransition, SavedTransition};
 use crate::file_io::{create_file_buf_write, has_data_left, open_file_buf_read};
-use std::collections::HashMap;
 use std::path::Path;
 use std::rc::Rc;
-
-fn transitions_serialized<'a, I>(
-    values: I,
-) -> (Vec<&'a Rc<CompressedImageOwned2>>, Vec<SavedTransition>)
-where
-    I: IntoIterator<Item = &'a CompressedTransition>,
-{
-    let mut frames = vec![];
-    let mut transitions: Vec<SavedTransition> = vec![];
-    let mut frame_pointers_to_indices = HashMap::new();
-    let mut current_frame_index = 0;
-    for transition in values {
-        let mut state_frame_indices = vec![];
-        for frame in transition.state.frames() {
-            let frame_index =
-                if let Some(frame_index) = frame_pointers_to_indices.get(&Rc::as_ptr(frame)) {
-                    *frame_index
-                } else {
-                    frames.push(frame);
-                    frame_pointers_to_indices.insert(Rc::as_ptr(frame), current_frame_index);
-                    let frame_index = current_frame_index;
-                    current_frame_index += 1;
-                    frame_index
-                };
-            state_frame_indices.push(frame_index);
-        }
-        let mut next_state_frame_indices = vec![];
-        for frame in transition.next_state.frames() {
-            let frame_index =
-                if let Some(frame_index) = frame_pointers_to_indices.get(&Rc::as_ptr(frame)) {
-                    *frame_index
-                } else {
-                    frames.push(frame);
-                    frame_pointers_to_indices.insert(Rc::as_ptr(frame), current_frame_index);
-                    let frame_index = current_frame_index;
-                    current_frame_index += 1;
-                    frame_index
-                };
-            next_state_frame_indices.push(frame_index);
-        }
-        let state_frame_indices: [usize; 4] = state_frame_indices.try_into().unwrap();
-        let next_state_frame_indices: [usize; 4] = next_state_frame_indices.try_into().unwrap();
-        let transition = SavedTransition {
-            state: state_frame_indices.into(),
-            next_state: next_state_frame_indices.into(),
-            action: transition.action,
-            reward: transition.reward,
-            terminated: transition.terminated,
-        };
-        transitions.push(transition);
-    }
-    (frames, transitions)
-}
+use transition_serializer::TransitionSerializer;
 
 pub fn save_transitions<'a, P, I>(path: P, transitions: I)
 where
@@ -63,7 +12,7 @@ where
     I: IntoIterator<Item = &'a CompressedTransition>,
 {
     let path = path.as_ref();
-    let (serialized_frames, serialized_transitions) = transitions_serialized(transitions);
+    let (serialized_frames, serialized_transitions) = TransitionSerializer::new().run(transitions);
 
     // the experience replay queue can take up a lot of space, therefore we save each
     // frame/transition separately in a streaming manner so as to not inadvertently clone
