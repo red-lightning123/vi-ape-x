60c205d79de6b49ecf67b3f10952a08fe4ec52c2
redlightning <redlightning_user@protonmail.com>
2024-05-29 07:11:29 +0300

    Add skeleton for replay server

--- replay/src/main.rs
+++ replay/src/main.rs
@@ -1,3 +1,16 @@
+use packets::ReplayRequest;
+use std::net::TcpListener;
+
 fn main() {
-    todo!("implementation");
+    let socket = TcpListener::bind("localhost:43430").unwrap();
+    loop {
+        let (stream, _source_addr) = socket.accept().unwrap();
+        let request = bincode::deserialize_from(&stream).unwrap();
+        match request {
+            ReplayRequest::ReleaseLock => todo!(),
+            ReplayRequest::SampleBatch { batch_len: _ } => todo!(),
+            ReplayRequest::InsertBatch { batch: _ } => todo!(),
+            ReplayRequest::UpdateBatchPriorities { batch: _ } => todo!(),
+        }
+    }
 }
