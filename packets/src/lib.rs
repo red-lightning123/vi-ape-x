2b493fb57f94284bf0fb5e9788a7807e522daae9
redlightning <redlightning_user@protonmail.com>
2024-05-29 06:59:51 +0300

    Derive serde traits for packet types

--- packets/src/lib.rs
+++ packets/src/lib.rs
@@ -1,19 +1,24 @@
 use replay_data::CompressedTransition;
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize)]
 pub enum LearnerRequest {
     GetParams,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct Insertion {
     pub transition: CompressedTransition,
     pub priority: f64,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct PriorityUpdate {
     pub index: usize,
     pub priority: f64,
 }
 
+#[derive(Serialize, Deserialize)]
 pub enum ReplayRequest {
     ReleaseLock,
     SampleBatch { batch_len: usize },
