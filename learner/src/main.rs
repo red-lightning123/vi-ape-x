4e86c16d462e359e4d0db5fa16299527caf02551
redlightning <redlightning_user@protonmail.com>
2024-05-30 19:59:16 +0300

    Add model to learner

--- learner/src/main.rs
+++ learner/src/main.rs
@@ -1,8 +1,10 @@
+use model::BasicModel;
 use packets::LearnerRequest;
 use std::net::TcpListener;
 
 fn main() {
     let socket = TcpListener::bind("localhost:43431").unwrap();
+    let _model = BasicModel::new();
     loop {
         let (stream, _source_addr) = socket.accept().unwrap();
         let request = bincode::deserialize_from(stream).unwrap();
