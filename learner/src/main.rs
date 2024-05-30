67f3c9337bf6c35eca61206452ce24af89ddd9a9
redlightning <redlightning_user@protonmail.com>
2024-05-30 18:22:30 +0300

    Add skeleton for learner server

--- learner/src/main.rs
+++ learner/src/main.rs
@@ -1,3 +1,13 @@
+use packets::LearnerRequest;
+use std::net::TcpListener;
+
 fn main() {
-    todo!("implementation");
+    let socket = TcpListener::bind("localhost:43431").unwrap();
+    loop {
+        let (stream, _source_addr) = socket.accept().unwrap();
+        let request = bincode::deserialize_from(stream).unwrap();
+        match request {
+            LearnerRequest::GetParams => todo!(),
+        }
+    }
 }
